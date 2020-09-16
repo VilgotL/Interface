6ba54e0667c6f3f31001b6167fdcee73bee32093
VilgotL <54435285+VilgotL@users.noreply.github.com>
2020-09-16 09:18:30 +0200

    Add files via upload

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -10,39 +10,36 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            double höjd;
-            double bredd;
-            double radie;
             Console.Write("Skriv 1 om det är en rektangel, 2 om det är en triangel och 3 om det är en cirkel: ");
             int form = int.Parse(Console.ReadLine());
             if (form == 1)
             {
                 Console.Write("Höjd: ");
-                höjd = double.Parse(Console.ReadLine());
+                double höjd = double.Parse(Console.ReadLine());
                 Console.Write("Bredd: ");
-                bredd = double.Parse(Console.ReadLine());
-                Rectangle r = new Rectangle(bredd, höjd);
-                IShape sh = r as IShape;
+                double bredd = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                IShape sh = new Rectangle(bredd, höjd);
                 sh.Circumferance();
                 sh.Area();
             }
             if (form == 2)
             {
                 Console.Write("Höjd: ");
-                höjd = double.Parse(Console.ReadLine());
+                double höjd = double.Parse(Console.ReadLine());
                 Console.Write("Bredd: ");
-                bredd = double.Parse(Console.ReadLine());
-                Triangle t = new Triangle(bredd, höjd);
-                IShape sh = t as IShape;
+                double bredd = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                IShape sh = new Triangle(bredd, höjd);
                 sh.Circumferance();
                 sh.Area();
             }
             if (form == 3)
             {
                 Console.Write("Radie: ");
-                radie = double.Parse(Console.ReadLine());
-                Circle c = new Circle(radie);
-                IShape sh = c as IShape;
+                double radie = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                IShape sh = new Circle(radie);
                 sh.Circumferance();
                 sh.Area();
             }
